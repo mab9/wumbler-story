cc9bdbec762a647139700b600bd7bca5fc4c291e
marc-antoine <marcantoine.bruelhart@gmail.com>
2018-10-02 17:51:24 +0200

    Write Readme, add wumbler py files - Write project goal and descriptions - add python scripts to calibrate, gather and transform  wumbler data from raspberry pi

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # wumbler-story
-Das Ziel dieses Projekts ist es, einen Datensatz zu ’sammeln’ und zu beschreiben, allenfalls daraus eine zuverlässige Messmethode für das Wumbler Projekt zu erstellen.
+Das Ziel dieses Projekts ist es, einen Datensatz zu ’sammeln’ und zu beschreiben, allenfalls daraus eine zuverlässige Messmethode für das Wumbler Projekt zu erstellen. 
+
+Damit das Ziel erreicht werden kann, werden mehrere Messungen der Bewegungen einer Waschmaschiene erstellt. Die Messdaten werden anschliessend aufbereitet und sollen eine Aussage über den 
+Waschmaschienen Zustand wiedergeben.  
+Dafür soll eine gute Messmethode mit aussagekräftige Resultaten gefunden werden.  
+
+
+## Werkzeuge
+Als Werkzeuge wird ein Raspberry PI Modell X und dem MPU-6050 Sensor verwendet.
+Das Setup wurde mit der Anleitung im PDF oder WWW xxx gemacht.
+
+
+## Datenquelle und Waschmaschiene
+Waschmaschiene, Typ, Zustand, etc
+
+## Messmethode
+Gemessen wird jeweils an bestimmten Punkte der Waschmaschiene.
+
+## Resultate
+
+ 
+
---
+++ sensor.py
@@ -0,0 +1,62 @@
+#! /usr/bin/python
+
+import time
+import smbus
+import math
+import requests
+import uuid
+
+# Register
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+
+# accolemeter config
+ACCEL_2G = 0x00
+ACCEL_4G = 0x08
+ACCEL_8G = 0x10
+ACCEL_16G = 0x18
+
+# Scale Modifiers
+ACCEL_SCALE_MODIFIER_2G = 16384.0
+ACCEL_SCALE_MODIFIER_4G = 8192.0
+ACCEL_SCALE_MODIFIER_8G = 4096.0
+ACCEL_SCALE_MODIFIER_16G = 2048.0
+
+def read_byte(reg):
+    return bus.read_byte_data(address, reg)
+
+
+def read_word(reg):
+    h = bus.read_byte_data(address, reg)
+    l = bus.read_byte_data(address, reg + 1)
+    value = (h << 8) + l
+    return value
+
+
+def read_word_2c(reg):
+    val = read_word(reg)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+
+bus = smbus.SMBus(1)
+address = 0x68
+
+# Aktivieren, um das Modul ansprechen zu koennen
+
+bus.write_byte_data(address, power_mgmt_1, 0)
+bus.write_byte_data(address, power_mgmt_1,ACCEL_2G)
+
+
+minute = 1
+seconds = 60
+timeout = time.time() + seconds * minute
+
+#filename = time.strftime("%Y%m%d-%H:%M")
+def readAcc():  
+     data = []
+     data.append(abs(read_word_2c(0x3b)))
+     data.append(abs(read_word_2c(0x3d)))
+     data.append(abs(read_word_2c(0x3f)))
+     return data
