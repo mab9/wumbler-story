2bb8663c37ef6414da2a0ecefb7826c14cb7f2f1
Marc-Antoine <marcantoine.bruelhart@gmail.com>
2018-10-09 17:16:40 +0200

    remove unused code - the code was used to push data to the server

--- wumbler.py
+++ wumbler.py
@@ -3,58 +3,36 @@
 import time
 import uuid
 
-import requests
-
 import calibrator
 import sensor
 
+#https://mlhub.cs.technik.fhnw.ch/user/marcantoine.bruelhart/lab - rQ3nzu
+
 laundryId = uuid.uuid4()
-serverUrl = "http://192.168.1.103:8080/"
 calibration = None
+dataPath = "datum.wd"
 
 
 def startWumblering(laundryId):
-    print("laundry with id: ", laundryId, " has started")
-    while True:
+    print("laundry: ", laundryId, " has started")
+    file = open(dataPath, "a")
 
-        data = []
-        data.append(laundryId)
-        data.append("&")
+    while True:
+        data = [laundryId, "&"]
 
         for _ in range(10):
             data.append(sensor.readAcc())
             data.append("&")
             time.sleep(0.1)
 
-        pushData(data)
+        file.write("Now the file has one more line!")
 
-
-def pushData(data):
-    try:
-        response = requests.post(serverUrl + 'collector', data={'data': data})
-        if response.status_code == 422:
-            print('[!] [{0}] wumbler not calibrated, try to submit calibration: [{1}]'.format(response.status_code,
-                                                                                              laundryId))
-            global calibration
-            pushCalibration(calibration)
-    except requests.exceptions.RequestException as e:
-        now = time.ctime(int(time.time()))
-        print("could not send sensor data to the server (connection error). " + str(now))
 
 
 def calibrateWumbler(laundryId):
     global calibration
     calibration = calibrator.calibrate(laundryId, 4)
-    pushCalibration(calibration)
 
 
-def pushCalibration(calibration):
-    try:
-        requests.post(serverUrl + 'collector/calibration', data={"data": calibration})
-    except requests.exceptions.RequestException as e:
-        now = time.ctime(int(time.time()))
-        print("could not send calibration data to the server (connection error). " + str(now))
-
-
-calibrateWumbler(laundryId)
+#calibrateWumbler(laundryId)
 startWumblering(laundryId)
